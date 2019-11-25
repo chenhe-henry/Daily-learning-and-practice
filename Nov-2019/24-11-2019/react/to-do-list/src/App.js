3b4048543ff13da796ce393c5967ca18ea64ab0c
Chen He <chenhe.elec@gmail.com>
2019-11-25 23:37:04 +1100

    useful extensions

---
+++ Nov-2019/24-11-2019/react/to-do-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>To do list</h1>
+      <button onClick>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ Nov-2019/24-11-2019/react/to-do-list/src/New.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class New extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Hello
+            </div>
+         );
+    }
+}
+ 
+export default New;
