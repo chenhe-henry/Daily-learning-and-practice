2a41ace13aee53f3086f689baa78782b947d5c1d
Chen He <chenhe.elec@gmail.com>
2019-11-30 00:57:48 +1100

    get react bootstrap

--- Nov-2019/24-11-2019/react/to-do-list/src/App.js
+++ Nov-2019/24-11-2019/react/to-do-list/src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
 
-function App() {
-  return (
-    <div>
-      <h1>To do list</h1>
-      <button onClick>Click Me!</button>
-    </div>
-  );
+        <h1>Hello</h1>
+        <Button variant="primary">Click</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
